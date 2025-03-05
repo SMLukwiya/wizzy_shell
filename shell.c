ccbafc3fe5e02e21842360323d4c6f2c7ed564b1
SMLukwiya <sundaymorganl@gmail.com>
2025-03-06 01:58:28 +0300

    new shell

--- shell.c
+++ shell.c
@@ -1,183 +1,118 @@
-#include "shell.h"
+#include <ctype.h>
+#include <errno.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-const char *builtin_commands[] = {
-  "cd", "echo", "jobs", "fg", "kill", "wait", "disown", "pwd", "export", "unset", "alias", "unalias", "set",
-  "exec", "printf", "read", "test", "if", "then", "else", "fi", "case", "for", "while", "until", "break",
-  "continue", "return", "exit", "help", "history", "type", "ulimit", "source", "."};
+#define MAXLINE 8192
+#define MAXARGS 128
 
-char *common_paths[] = {"/bin", "/usr/bin", "/usr/local/bin", NULL};
+extern char **environ;
 
 void unix_error(char *msg) {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(0);
 }
 
-char *Fgets(char *ptr, int n, FILE *stream) {
-    if (fgets(ptr, n, stream) == NULL)
-        unix_error("fgets error");
-    return ptr;
-}
-
-pid_t Fork() {
-    pid_t pid;
-
-    if ((pid = fork()) < 0)
-        unix_error("Fork error");
-
-    return pid;
-}
-
-int parseline(char *buf, char **argv) {
-    char *delim; /*First delimiter*/
-    int argc;    /*Number of argv items*/
-    int bg;      /* Background process?*/
-
-    buf[strlen(buf) - 1] = ' '; /*replace trailing ' ' */
-    while (*buf && *buf == ' ') /*Ignore leading space*/
-        buf++;
-
-    argc = 0;
-    while ((delim = strchr(buf, ' '))) {
-        *delim = '\0'; /* replace current delimiter */
-        argv[argc++] = buf;
-        buf = delim + 1; /*Move buf to next string*/
-        while (*buf && *buf == ' ')
-            buf++;
-    }
-
-    argv[argc] = NULL;
-    if (argc == 0)
-        return 1;
-
-    if ((bg = (*argv[argc - 1] == '&')) != 0)
-        argv[--argc] = NULL;
-
-    return bg;
-}
-
-void eval(char *cmdline) {
-    char *argv[MAXARGS];
-    char buf[MAXLINE];
-    char final_command[MAXARGS];
-    int bg;
-    pid_t pid;
-
-    strcpy(buf, cmdline);
-    bg = parseline(buf, argv);
-    if (argv[0] == NULL) /* Ignore empty lines*/
-        return;
-
-    if (!builtin_command(argv)) {
-        if ((pid = Fork()) == 0) {
-            if (find_command(argv[0], final_command) < 0) {
-                printf("%s: command not found.\n", final_command);
-                exit(0);
-            } else {
-                if (execve(final_command, argv, environ) < 0) {
-                    printf("%s: command not found.\n", final_command);
-                    exit(0);
-                }
-            }
-        }
-
-        if (!bg) {
-            int status;
-            if (waitpid(pid, &status, 0) < 0)
-                unix_error("waitfg: waiting error");
-        } else {
-            printf("%d %s\n", pid, cmdline);
-        }
-    }
-    return;
-}
-
-/* Check in dir */
-int find_in_dir(char *dirname, char *command, char *final_command) {
-    sprintf(final_command, "%s/%s", dirname, command);
-    if (access(final_command, X_OK) == 0) {
-        return 0;
-    }
-
-    final_command = NULL;
-    return -1;
-}
-
-int find_command(char *command, char *final_command) {
-    char *paths;
-    const char *delimiter = ":";
-    int i;
-
-    for (i = 0; common_paths[i]; i++) {
-        char *res;
-        if (find_in_dir(common_paths[i], command, final_command) == 0) {
-            return 0;
-        };
-    }
-
-    if (!(paths = getenv("PATH"))) {
-        final_command = NULL;
-        return -1;
-    }
-
-    char *token;
-    token = strtok(paths, delimiter);
-
-    while (token != NULL) {
-        char *res;
-        if (find_in_dir(token, command, final_command) == 0) {
-            return 0;
-        };
-        token = strtok(NULL, delimiter);
-    }
-
-    final_command = NULL;
-    return -1;
-}
-
-int builtin_command(char **argv) {
-    if (!strcmp(argv[0], "quit")) /*Quit command*/
-        exit(0);
-
-    if (!strcmp(argv[0], "&")) /*Ignore singleton &*/
-        return 1;
-
-    return 0;
-}
-
-/* shell prompt structure */
-int get_shell_prompt(char *buf) {
+void shell_prompt(char *shell_p) {
     char hostname[MAXARGS];
     char cwd[MAXARGS];
     struct passwd *user;
     user = getpwuid(geteuid());
 
-    if (gethostname(hostname, MAXARGS) < 0) {
-        printf("%s\n", "Error");
-    }
+    if (gethostname(hostname, MAXARGS) < 0)
+        sprintf(hostname, " ");
 
     if (!getcwd(cwd, MAXARGS))
-        printf("Error\n");
+        sprintf(cwd, " ");
 
-    int home_dir_length = strlen(user->pw_dir);
-    if (!strncmp(user->pw_dir, cwd, home_dir_length))
-        strcpy(cwd, &cwd[home_dir_length]);
+    int home_dir_len = strlen(user->pw_dir);
+    if (!strncasecmp(user->pw_dir, cwd, home_dir_len))
+        strcpy(cwd, &cwd[home_dir_len]);
 
-    sprintf(buf, "\x1b[1;34m%s@%s\x1b[0;37m:\x1b[1;32m~%s", user->pw_name, hostname, cwd);
-    return 0;
+    sprintf(shell_p, "\x1b[1;32m%s@%s\x1b[0;37m:\x1b[1;34m~%s\x1b[0;37m", user->pw_name, hostname, cwd);
 }
 
-int main() {
-    char cmdline[MAXLINE];
-    char buf[MAXARGS * 3];
+void trim_whitespace(char **start) {
+    char *end;
+    while (isspace((unsigned char)**start))
+        (*start)++;
 
-    get_shell_prompt(buf);
-    while (1) {
-        /* Read */
-        printf("%s$ ", buf);
-        Fgets(cmdline, MAXLINE, stdin);
+    /* empty command */
+    if (**start == '\0')
+        return;
+
+    end = *start + strlen(*start) - 1;
+    while (end > *start && isspace((unsigned char)*end))
+        end--;
+    *(end + 1) = '\0';
+}
+
+int parse_command(char *command, char **argv) {
+    char *token;
+    char *delimiter = " \t";
+    int argc, background;
+
+    trim_whitespace(&command);
+
+    /* empty command */
+    if (*command == '\0')
+        return 1;
+
+    token = strtok(command, delimiter);
+
+    argc = 0;
+    while (token != NULL) {
+        argv[argc++] = token;
+        token = strtok(NULL, delimiter);
+    }
+
+    if ((background = (*argv[argc - 1]) == '&'))
+        argc--;
+    argv[argc] = NULL;
+    return background;
+}
+
+void eval_command(char *command) {
+    char *argv[MAXARGS];
+    int background;
+    pid_t pid;
+
+    background = parse_command(command, argv);
+
+    if (argv[0] == NULL)
+        return;
+
+    if ((pid = fork()) < 0)
+        unix_error("fork");
+
+    if (pid == 0) {
+        if (execvp(argv[0], argv) < 0)
+            unix_error("execvp");
+    } else {
+        if (!background) {
+            waitpid(pid, NULL, 0);
+        }
+        return;
+    }
+}
+
+int main(int argc, char **argv) {
+    int should_run = 1;
+    char command[MAXLINE];
+    char shell_p[MAXLINE];
+
+    shell_prompt(shell_p);
+    while (should_run) {
+        printf("%s> ", shell_p);
+        fgets(command, MAXLINE, stdin);
         if (feof(stdin))
             exit(0);
-
-        eval(cmdline);
+        eval_command(command);
     }
 }
